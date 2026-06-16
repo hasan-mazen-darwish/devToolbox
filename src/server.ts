6d1cad8582d5720b816d0400ad404836504763a3
Hasan Darwish <zceline76@gmail.com>
2026-06-16 23:11:35 +0300

    Added dependency dotenv, made imports possible, and added an initial code to server.ts

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import { config as dotEnvConfig } from "dotenv"
+
+dotEnvConfig()
+const app = express()
+const PORT: Number = Number(process.env.PORT) || 7000
+
+app.get("/", (req, res) => {
+  res.send("Hello from \"/\"")
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running successfully on port ${PORT}.`)
+})
