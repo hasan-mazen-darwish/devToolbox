5ab17e43bffde1523ebae285def77c8bc10f5ea4
Hasan Darwish <zceline76@gmail.com>
2026-06-17 21:59:11 +0300

    Added some DX related code to the console.log of the app.listen

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,7 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT, () => {
+  console.log(`--------------------------------------------`)
   console.log(`App is running successfully on port ${PORT}.`)
+  console.log("\n")
 })
