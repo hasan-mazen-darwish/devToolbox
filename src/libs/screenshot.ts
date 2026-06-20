0fe0d39cc8bb78e3c8cd686c1afd3497637c2f77
Hasan Darwish <zceline76@gmail.com>
2026-06-20 22:41:20 +0300

    Added the option of the image file extension in the screenshot function options

--- src/libs/screenshot.ts
+++ src/libs/screenshot.ts
@@ -2,7 +2,8 @@ import type { Browser, Page } from "puppeteer"
 
 interface ScreenshotOptions {
   url: string
-  option?: "buffer" | "base64"
+  option?: "buffer" | "base64",
+  imageExtension?: "webp" | "png" | "jpeg"
 }
 
 /**
@@ -15,6 +16,8 @@ interface ScreenshotOptions {
  * `options.url`: **required**. the URL given to the function so it can copy it.
  * 
  * `options.option`: *optional*. takes the value of either `buffer` or `base64`. default is `buffer`.
+ * 
+ * `options.imageExtension`: *optional*. takes three values: `webp`, `png` or `jpeg`. this defines the type of the image (as follows). default is `webp`
  * 
  * @example
  * import puppeteer from "puppeteer";
@@ -43,7 +46,7 @@ async function takeScreenshot(browser: Browser, options: ScreenshotOptions): Pro
       const screenshot = await page.screenshot({
         fullPage: true,
         encoding: options.option ? (options.option == "buffer" ? "binary" : "base64") : "binary",
-        type: "webp"
+        type: options.imageExtension || "webp"
       })
 
       return screenshot as Buffer | string
