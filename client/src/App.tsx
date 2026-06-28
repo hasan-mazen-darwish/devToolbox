2e6113a035612cbbb152f06a0eeb83007e87bac9
Hasan Darwish <zceline76@gmail.com>
2026-06-28 10:57:59 +0300

    Added routes, theme, and some minor fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,47 @@
-import './App.css'
 import CssBaseLine from "@mui/material/CssBaseline"
 
 import AppBar from './components/landpage/AppBar'
+import { Route, Routes } from "react-router-dom"
+import { Container, createTheme, ThemeProvider } from "@mui/material"
 
 function App() {
-  return (
-    <>
-      <CssBaseLine enableColorScheme />
+  const theme = createTheme({
+    palette: {
+      mode: "light",
       
+      primary: {
+        main: "#3F51B5",
+        dark: "#303F9F",
+        light: "#C5CAE9",
+        contrastText: "#FFFFFF"
+      },
+
+      secondary: {
+        main: "#03A9F4",
+      },
+
+      text: {
+        primary: "#212121",
+        secondary: "#757575"
+      },
+
+      divider: "#BDBDBD"
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseLine enableColorScheme />
       <AppBar />
-    </>
+      <Container sx={{paddingTop: "100px"}}></Container>
+
+      <Routes>
+        <Route path="/">
+          <Route index element={<></>}></Route>
+        </Route>
+        <Route path="*"></Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
 
