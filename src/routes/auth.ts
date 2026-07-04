5c771836744379ed30dd662062b579a8c0f53d17
Hasan Darwish <zceline76@gmail.com>
2026-07-04 21:28:24 +0300

    fixed validator import problem

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import supabase from "../libs/supabase"
 import { stringer } from "../libs/utils"
-import { isEmail, isStrongPassword } from "validator"
+import validator from "validator"
 import { errorResponser, invalidInputResponser, responser } from "../libs/routeFunctions/responser"
 
 const route = express.Router()
@@ -14,13 +14,13 @@ route.post("/signup", async (req, res) => {
 
   const sanitizedEmail = stringer(email, {maximumCap: 254})
   if(!sanitizedEmail) return invalidInputResponser(res, {errorCode: "emailNotProvided"})
-  if(!isEmail(sanitizedEmail)) return invalidInputResponser(res, {errorCode: "emailNotValid"})
+  if(!validator.isEmail(sanitizedEmail)) return invalidInputResponser(res, {errorCode: "emailNotValid"})
 
   const sanitizedPassword = stringer(password, {maximumCap: 101})
   if(!sanitizedPassword) return invalidInputResponser(res, {errorCode: "passwordNotProvided"})
   if(sanitizedPassword.length > 100) return invalidInputResponser(res, {errorCode: "passwordTooLong"})
   if(sanitizedPassword.length < 8) return invalidInputResponser(res, {errorCode: "passwordTooShort"})
-  if(!isStrongPassword(sanitizedPassword, {
+  if(!validator.isStrongPassword(sanitizedPassword, {
     minLength: 8,
     minLowercase: 1,
     minNumbers: 1,
